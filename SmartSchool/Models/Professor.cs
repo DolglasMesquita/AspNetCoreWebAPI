b44899cea9e6048932d61bdc92086264caacacf5
Dolglas Mesquita <dolglas_mesquita@hotmail.com>
2021-02-03 17:54:28 -0300

    Expansão dos models e automapper

--- SmartSchool/Models/Professor.cs
+++ SmartSchool/Models/Professor.cs
@@ -2,23 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
+using System.ComponentModel.DataAnnotations;
 
 namespace SmartSchool.Models
 {
     public class Professor
     {
         public int Id { get; set; }
+        public int Registro { get; set; }
         public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataIni { get; set; } = DateTime.Now;
+        public DateTime? DataFim { get; set; } = null;
+        public bool Ativo { get; set; } = true;
         public IEnumerable<Disciplina> Disciplinas { get; set; }
 
         public Professor()
         {
         }
 
-        public Professor(int id, string nome)
+        public Professor(int id, int registro, string nome, string sobrenome)
         {
             Id = id;
+            Registro = registro;
             Nome = nome;
+            Sobrenome = sobrenome;
         }
     }
 }
